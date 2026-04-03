f26add0d93daada87941d9fa6b4ab1505e20c599
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-03 15:12:19 +0530

    feat(product): integrated create product api

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -1,5 +1,6 @@
 import type { GetProductsParams, GetProductsResponse } from '@/types/product.types';
 import { api } from './axios'; // your interceptor
+import type { CreateProductFormValues } from '@/schemas/product.schema';
 
 export const getProducts = async (params: GetProductsParams): Promise<GetProductsResponse> => {
   const { data } = await api.get('/product', {
@@ -7,4 +8,9 @@ export const getProducts = async (params: GetProductsParams): Promise<GetProduct
   });
 
   return data;
+};
+
+export const createProduct = async (data: CreateProductFormValues) => {
+  const res = await api.post('/product', data);
+  return res.data;
 };
