33eec3b78e027c8744d304fa503700e4cd8af111
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-04 18:50:03 +0530

    feat: invalidate products data to fetch new data after create a product

--- src/hooks/products/useCreateProduct.ts
+++ src/hooks/products/useCreateProduct.ts
@@ -1,8 +1,14 @@
 import { createProduct } from '@/api/product.api';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useCreateProduct = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createProduct,
+    onSuccess: () => {
+      // Invalidate the products query to refetch the latest data
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
   });
 };
